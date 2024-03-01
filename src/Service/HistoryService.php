c9608fd2e43be47bb862fe59702698700ae4979d
saidbenhmed3 <saidbenhmed3@gmail.com>
2024-03-01 11:09:36 +0100

    rectifs history

--- src/Service/HistoryService.php
+++ src/Service/HistoryService.php
@@ -11,21 +11,24 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class HistoryService
 {
     private $entityManager;
     private $tokenService;
     private $ImageService;
-
     private $parameterBag;
+    private $serializer;
 
-    public function __construct(EntityManagerInterface $entityManager,TokenService $tokenService,ImageService $ImageService, ParameterBagInterface $parameterBag)
+    public function __construct(SerializerInterface $serializer,EntityManagerInterface $entityManager,TokenService $tokenService,ImageService $ImageService, ParameterBagInterface $parameterBag)
     {
         $this->entityManager = $entityManager;
         $this->tokenService = $tokenService;
         $this->ImageService = $ImageService;
         $this->parameterBag = $parameterBag;
+        $this->serializer = $serializer;
     }
     public function getHistoryList($request)
     {
@@ -59,91 +62,8 @@ class HistoryService
 
             if ($inventaire->getArbre())
             {
-            $arbre = $inventaire->getArbre();
-            $history = new History();
-
+            $this->serializer->deserialize(json_encode($inventaire->getArbre(), true), History::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $history]);
             $history->setInventaire($inventaire);
-            $history->setEspece($arbre->getEspece());
-            $history->setDiametre($arbre->getDiametre());
-            $history->setCreatedAt($arbre->getCreatedAt());
-            $history->setUpdatedAt($arbre->getUpdatedAt());
-            $history->setCoord($arbre->getCoord());
-            $history->setUser($arbre->getUser());
-            $history->setCodeSite($arbre->getCodeSite());
-            $history->setNumSujet($arbre->getNumSujet());
-            $history->setCritere($arbre->getCritere());
-            $history->setImplantation($arbre->getImplantation());
-            $history->setDomaine($arbre->getDomaine());
-            $history->setNuisance($arbre->getNuisance());
-            $history->setProximite($arbre->getProximite());
-            $history->setTauxFreq($arbre->getTauxFreq());
-            $history->setTypePassage($arbre->getTypePassage());
-            $history->setAccessibilite($arbre->getAccessibilite());
-            $history->setAbattage($arbre->getAbattage());
-            $history->setTravauxCollet($arbre->getTravauxCollet());
-            $history->setTravauxTronc($arbre->getTravauxTronc());
-            $history->setTravauxHouppier($arbre->getTravauxHouppier());
-            $history->setDateTravaux($arbre->getDateTravaux());
-            $history->setDateProVisite($arbre->getDateProVisite());
-            $history->setComProVisite($arbre->getComProVisite());
-            $history->setCaractPied($arbre->getCaractPied());
-            $history->setCaractTronc($arbre->getCaractTronc());
-            $history->setHauteur($arbre->getHauteur());
-            $history->setPortArbre($arbre->getPortArbre());
-            $history->setStadeDev($arbre->getStadeDev());
-            $history->setEtatSanCollet($arbre->getEtatSanCollet());
-            $history->setEtatSanTronc($arbre->getEtatSanTronc());
-            $history->setEtatSanHouppier($arbre->getEtatSanHouppier());
-            $history->addNuisanceNuisibles($arbre->getNuisanceNuisibles());
-            $history->setAddress($arbre->getAddress());
-            $history->setComAccess($arbre->getComAccess());
-            $history->setDict($arbre->getDict());
-            $history->setRisque($arbre->getRisque());
-            $history->setProximiteOther($arbre->getProximiteOther());
-            $history->setAccessibiliteOther($arbre->getProximiteOther());
-            $history->setCaractPiedOther($arbre->getCaractPiedOther());
-            $history->setCaractTroncMultiples($arbre->getCaractTroncMultiples());
-            $history->setEtatSanColletCavite($arbre->getEtatSanColletCavite());
-            $history->setEtatSanColletChampignons($arbre->getEtatSanColletChampignons());
-            $history->setEtatSanTroncCavite($arbre->getEtatSanTroncCavite());
-            $history->setEtatSanTroncCorpsEtranger($arbre->getEtatSanTroncCorpsEtranger());
-            $history->setEtatSanTroncChampignons($arbre->getEtatSanTroncChampignons());
-            $history->setEtatSanTroncNuisibles($arbre->getEtatSanTroncNuisibles());
-            $history->setEtatSanHouppierChampignons($arbre->getEtatSanHouppierChampignons());
-            $history->setEtatSanHouppierNuisibles($arbre->getEtatSanHouppierNuisibles());
-            $history->setRisqueGeneral($arbre->getRisqueGeneral());
-            $history->setTravauxCommentaire($arbre->getTravauxCommentaire());
-            $history->setCritereOther($arbre->getCritereOther());
-            $history->setRisqueGeneralOther($arbre->getRisqueGeneralOther());
-            $history->setTypePassageOther($arbre->getTypePassageOther());
-            $history->setTypeIntervention($arbre->getTypeIntervention());
-            $history->setEtatSanGeneral($arbre->getEtatSanGeneral());
-            $history->setUserEditedDateTravaux($arbre->getUserEditedDateTravaux());
-            $history->setTravauxTroncOther($arbre->getTravauxTroncOther());
-            $history->setTravauxColletOther($arbre->getTravauxColletOther());
-            $history->setTravauxHouppierOther($arbre->getTravauxHouppierOther());
-            $history->setProximiteWithDict($arbre->getProximiteWithDict());
-            $history->setVille($arbre->getVille());
-            $history->setPays($arbre->getPays());
-            $history->setTravauxTroncProtection($arbre->getTravauxTroncProtection());
-            $history->setImg1($arbre->getImg1());
-            $history->setImg2($arbre->getImg2());
-            $history->setImg3($arbre->getImg3());
-            $history->setEtatSanTroncNuisiblesAutres($arbre->getEtatSanTroncNuisiblesAutres());
-            $history->setEtatSanHouppierNuisiblesAutres($arbre->getEtatSanHouppierNuisiblesAutres());
-            $history->setTravauxColletMultiple($arbre->getTravauxColletMultiple());
-            $history->setTravauxTroncMultiple($arbre->getTravauxTroncMultiple());
-            $history->setTravauxHouppierMultiple($arbre->getTravauxHouppierMultiple());
-            $history->setStatusTravaux($arbre->getStatusTravaux());
-            $history->setEtatSanColletChampignonsAutres($arbre->getEtatSanColletChampignonsAutres());
-            $history->setEtatSanTroncChampignonsAutres($arbre->getEtatSanTroncChampignonsAutres());
-            $history->setEtatSanHouppierChampignonsAutres($arbre->getEtatSanHouppierChampignonsAutres());
-            $history->setEtatSanColletOther($arbre->getEtatSanColletOther());
-            $history->setEtatSanTroncOther($arbre->getEtatSanTroncOther());
-            $history->setEtatSanHouppierOther($arbre->getEtatSanHouppierOther());
-            $history->setEtatSanGeneralOther($arbre->getEtatSanGeneralOther());
-
-            $this->entityManager->persist($history);
             $this->entityManager->flush();
             }
 
